5fa5788c9d928a9162c457548f76698d76dedd45
bhavinklx <kalena.bhavin@gmail.com>
2026-01-23 17:28:19 +0530

    Logo related change

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,5 +1,6 @@
 <!-- App footer starts -->
 <div class="app-footer bg-white">
-    <span>&copy; Medflex admin {{ date('Y') }}</span>
+    &copy; {{ date('Y') }} Sensify Care.
+    <span class="text-end">Designed & Developed by Om Net Solution.</span>
 </div>
 <!-- App footer ends -->
